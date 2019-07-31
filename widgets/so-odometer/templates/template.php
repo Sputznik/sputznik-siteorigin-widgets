08710bad08831b075ac0c3239e826dbe178c799c
Steve <anil.stephen66@gmail.com>
2019-07-31 15:36:46 +0530

    Added separate js file for odometer

--- widgets/so-odometer/templates/template.php
+++ widgets/so-odometer/templates/template.php
@@ -5,7 +5,7 @@
   global $sp_sow;
   $widget_id = 'sow-odometer-'.$sp_sow->getUniqueID( $instance );
   ?>
-      	<div class="odometer-wrapper" id="<?php _e( $widget_id ); ?>">
+      	<div class="odometer-wrapper" data-delay='<?php _e( $odometer_delay );?>' data-behaviour="sp-odometer"  id="<?php _e( $widget_id ); ?>">
           <?php
           foreach ($odometer_repeater as $value) {
             // _e( $value['icon_color'] );
@@ -27,14 +27,6 @@
   grid-template-columns: repeat( <?php _e( $instance['odometer_normal'] ); ?> ,1fr);
   grid-gap: 10px;
   height:100%;
-  /* background:#ebebeb; */
-}
-.odometer-wrapper .odometer-Txt{
-  text-align:center;
-  /* font-size:40px; */
-  font-weight:bold;
-  text-transform:uppercase;
-  padding: 10px;
 }
 @media(max-width:767px){
   .odometer-wrapper{
@@ -48,60 +40,13 @@
     grid-template-columns: repeat( <?php _e( $instance['odometer_tablet'] ); ?> ,1fr);
   }
 }
-.odometer-wrapper .odometer-Txt span{display:inline-block;}
 .odometer-wrapper .odometer-suffix,.odometer-wrapper .odometer-prefix{
   font-size: <?php _e($instance['odometer_font'])?>;
 }
 .odometer-value{font-size: <?php _e($instance['odometer_font'])?>}
-.odometer-desc span{
-  color: #ccc;
-}
-.odometer-desc{
-  color: #888;
-}
-.odometer-desc .sow-fas{
-  margin-right: 15px;
-  font-size: 32px;
-  /* float: left; */
-  font-weight: 900;
-}
+
 /* @media(min-width:320px) and (max-width:767px){
 .c-no{height:100%;}
 .counter-Txt{margin-top:35px;}
 .margin-bot-35{margin-bottom:35px;} */
 </style>
-<script>
-var a = 0;
-jQuery(window).scroll(function() {
-
-  var oTop = jQuery('#<?php _e( $widget_id ); ?>').offset().top - window.innerHeight;
-  if (a == 0 && jQuery(window).scrollTop() > oTop) {
-    jQuery('.odometer-value').each(function() {
-      var $this = jQuery(this),
-        countTo = $this.attr('data-count');
-        console.log('hiiii');
-      jQuery({
-        countNum: $this.text()
-      }).animate({
-          countNum: countTo
-        },
-
-        {
-
-          duration: <?php _e( $instance['odometer_delay'] ); ?>,
-          easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-            //alert('finished');
-          }
-
-        });
-    });
-    a = 1;
-  }
-
-});
-</script>
